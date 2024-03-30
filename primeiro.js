7dea1b9de68700e2b488abcae29c174a0af4ab3c
JonathanJoseGonzalez <110431669+JonathanJoseGonzalez@users.noreply.github.com>
2024-03-30 13:48:15 -0300

    Quebrando a Maldição em JavaScript

--- primeiro.js
+++ primeiro.js
@@ -1,5 +1,5 @@
-console.log ("Quebrando a maldição do 'Hello World'");
-console.log("He4rtDevs <3");
-console.log(123);
-console.log([1,2,3]);
-console.log({curso: "js4noobs"});
+// Definindo uma variável com uma mensagem
+var mensagem = "Olá, mundo!";
+
+// Imprimindo a mensagem no console
+console.log(mensagem);
